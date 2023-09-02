ae0086fec4f809378e01b7d59800ec20b8b5a5e6
ElvisAns <ansimapersic@gmail.com>
2023-09-02 16:41:08 +0200

    feat: send sensor data to server

--- Project_KG_1.ino
+++ Project_KG_1.ino
@@ -1,5 +1,11 @@
 #include "Arduino.h"
 #include "LM35.h"
+#include <WiFi.h> // Include the WiFi library
+
+// Define WiFi credentials
+const char* ssid = "YourWiFiSSID";
+const char* password = "YourWiFiPassword";
+const char* serverAddress = "http://your_server_endpoint"; // Replace with your server URL
 
 // Define pin assignments for sensors and components
 #define ADXL335_PIN_XOUT 12
@@ -37,11 +43,22 @@ void IRAM_ATTR onSampleTime() {
 // Flag to control sending PulseSensor Signal data to serial
 boolean sendPulseSignal = false;
 
+// WiFi client
+WiFiClient client;
+
 void setup() {
   // Initialize serial communication
   Serial.begin(115200);
   delay(1500);
   Serial.println("start");
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 
   // Configure PulseSensor
   pulseSensor.analogInput(HEARTPULSE_PIN_SIG);
@@ -79,4 +96,67 @@ void loop() {
   // Read acceleration components
   float ax, ay, az;
   accelerometer.getAcceleration(&ax, &ay, &az);
+
+  // Create JSON data
+  String jsonPayload = "{";
+  jsonPayload += "\"bpm\":" + bpm + ",";
+  jsonPayload += "\"temperature\":" + String(lm35TempC) + ",";
+  jsonPayload += "\"acceleration_x\":" + String(ax) + ",";
+  jsonPayload += "\"acceleration_y\":" + String(ay) + ",";
+  jsonPayload += "\"acceleration_z\":" + String(az);
+  jsonPayload += "}";
+
+  // Send data to server every 1 minute
+  static unsigned long previousMillis = 0;
+  const unsigned long interval = 60000; // 1 minute
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    // Send data to server
+    if (sendDataToServer(jsonPayload)) {
+      Serial.println("Data sent to server successfully");
+    } else {
+      Serial.println("Failed to send data to server");
+    }
+  }
+}
+
+bool sendDataToServer(String payload) {
+  if (client.connect(serverAddress, 80)) {
+    // Make an HTTP POST request
+    client.println("POST " + String(serverAddress) + " HTTP/1.1");
+    client.println("Host: " + String(serverAddress));
+    client.println("Content-Type: application/json");
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(payload.length());
+    client.println();
+    client.println(payload);
+
+    // Wait for the server to respond
+    delay(1000);
+
+    // Check HTTP status
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      if (line == "\n") {
+        break;
+      }
+    }
+
+    // Check for a successful response (HTTP status code 200)
+    if (client.find("HTTP/1.1 200 OK")) {
+      client.stop();
+      return true;
+    } else {
+      client.stop();
+      return false;
+    }
+  } else {
+    // Unable to connect to the server
+    client.stop();
+    return false;
+  }
 }
